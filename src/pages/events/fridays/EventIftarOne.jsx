4eef5fae24181ec1b1956825ac2137ae5852fb9a
saleckahmed <mohamedsaleck043@gmail.com>
2025-08-29 14:10:34 +0000

    update site

--- src/pages/events/fridays/EventIftarOne.jsx
+++ src/pages/events/fridays/EventIftarOne.jsx
@@ -1,5 +1,5 @@
 import IftarDetails from "../../../components/IftarDetails";
-import video from "../../../assets/Friday1/video1.mp4"
+import video from "../../../assets/Friday1/video.mp4"
 
 const EventIftarOne = () => {
   const eventData = {
@@ -8,12 +8,12 @@ const EventIftarOne = () => {
     description:
       "انطلقت فعاليات جمعة الخير الأولى بمشاركة واسعة من المتطوعين والمحتاجين. تم تقديم وجبات إفطار لـ 250 شخصاً في المسجد الجامع، مع برنامج ترفيهي وتوعوي للأطفال.",
     images: [
-      "https://images.unsplash.com/photo-1517232115160-ff93364542dd?ixlib=rb-4.0.3&auto=format&fit=crop&w=500&q=80",
-      "https://images.unsplash.com/photo-1512058564366-18510be2db19?ixlib=rb-4.0.3&auto=format&fit=crop&w=500&q=80",
-      "https://images.unsplash.com/photo-1605888236237-5bb7c32b6d70?ixlib=rb-4.0.3&auto=format&fit=crop&w=500&q=80",
-      "https://images.unsplash.com/photo-1596461404969-9ae70f2830c1?ixlib=rb-4.0.3&auto=format&fit=crop&w=500&q=80",
-      "https://images.unsplash.com/photo-1611606063065-ee7946e07526?ixlib=rb-4.0.3&auto=format&fit=crop&w=500&q=80",
-      "https://images.unsplash.com/photo-1611606063065-ee7946e07526?ixlib=rb-4.0.3&auto=format&fit=crop&w=500&q=80",
+      "https://www.redswastik.com/files/branches/Nocontent2.png",
+      "https://www.redswastik.com/files/branches/Nocontent2.png",
+      "https://www.redswastik.com/files/branches/Nocontent2.png",
+      "https://www.redswastik.com/files/branches/Nocontent2.png",
+      "https://www.redswastik.com/files/branches/Nocontent2.png",
+      "https://www.redswastik.com/files/branches/Nocontent2.png",
     ],
     video: video,
     
--- src/pages/events/fridays/EventIftarTwo.jsx
+++ src/pages/events/fridays/EventIftarTwo.jsx
@@ -1,4 +1,6 @@
 import IftarDetails from "../../../components/IftarDetails";
+import video from "../../../assets/Friday2/video.mp4";
+
 
 const EventIftarTwo = () => {
   const eventData = {
@@ -6,15 +8,15 @@ const EventIftarTwo = () => {
     date: "15 رمضان 1446",
     description:
       "شهدت الجمعة الثانية استمراراً لفعاليات الإفطار الجماعي مع إضافة برنامج توعوي عن أهمية الصحة الغذائية في رمضان. تم توزيع أكثر من 300 وجبة إفطار مع حقيبة تحتوي على مواد توعوية.",
-    images: [
-      "https://images.unsplash.com/photo-1512058564366-18510be2db19?ixlib=rb-4.0.3&auto=format&fit=crop&w=500&q=80",
-      "https://images.unsplash.com/photo-1473093295043-cdd812d0e601?ixlib=rb-4.0.3&auto=format&fit=crop&w=500&q=80",
-      "https://images.unsplash.com/photo-1543339301-9c5f6b5e4a90?ixlib=rb-4.0.3&auto=format&fit=crop&w=500&q=80",
-      "https://images.unsplash.com/photo-1464366400600-7168b8af9bc3?ixlib=rb-4.0.3&auto=format&fit=crop&w=500&q=80",
-      "https://images.unsplash.com/photo-1512058564366-18510be2db19?ixlib=rb-4.0.3&auto=format&fit=crop&w=500&q=80",
-      "https://images.unsplash.com/photo-1473093295043-cdd812d0e601?ixlib=rb-4.0.3&auto=format&fit=crop&w=500&q=80",
+   images: [
+      "https://www.redswastik.com/files/branches/Nocontent2.png",
+      "https://www.redswastik.com/files/branches/Nocontent2.png",
+      "https://www.redswastik.com/files/branches/Nocontent2.png",
+      "https://www.redswastik.com/files/branches/Nocontent2.png",
+      "https://www.redswastik.com/files/branches/Nocontent2.png",
+      "https://www.redswastik.com/files/branches/Nocontent2.png",
     ],
-    video: "https://www.youtube.com/embed/6stlCkUDG_s",
+    video: video,
   };
 
 
